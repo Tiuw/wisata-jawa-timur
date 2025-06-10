f5af1868591ea4681d3a08c53fb50dd5faf65b32
Tiuw <danendraalvyn@gmail.com>
2025-06-10 15:46:28 +0700

    update proxy configuration to point to the correct backend server and enhance server settings

--- src/setupProxy.js
+++ src/setupProxy.js
@@ -4,7 +4,7 @@ module.exports = function (app) {
   app.use(
     "/wisata_jawa_timur", // This is the API route that you want to proxy
     createProxyMiddleware({
-      target: "http://localhost", // Change this to your backend server
+      target: "http://localhost:8000", // Change this to your backend server
       changeOrigin: true, // This will help with CORS
       pathRewrite: {
         "^/wisata_jawa_timur": "/wisata_jawa_timur", // Ensure the path is correct
